59e2c8a8babd497845042427a4da8ae3621fa2e7
William Mata <wmata0141@gmail.com>
2022-05-01 18:30:13 -0400

    title

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <nav className='navbar navbar-expand-lg navbar-expand-xl navbar-dark bg-danger fixed-top'>
       <div className='container'>
-        <a className='navbar-brand' href='/#top'>React Movies</a>
+        <a className='navbar-brand' href='/'>React Movies</a>
         <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarResponsive' aria-controls='navbarResponsive' aria-expanded='false' aria-label='Toggle navigation'>
           <span className='navbar-toggler-icon'></span>
         </button>
